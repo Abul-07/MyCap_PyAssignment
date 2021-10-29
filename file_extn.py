0fe24943b1c595ebe477935c0ab7f075bc21746a
Abul-07 <92680357+Abul-07@users.noreply.github.com>
2021-10-29 20:24:02 +0530

    Removed repr( )
    I used repr( ) to add quote to the output

--- file_extn.py
+++ file_extn.py
@@ -1,3 +1,3 @@
 file_name = input("Input the file name: ")
 file_extn = file_name.split(".")
-print("The extension of the file is:", repr(file_extn[-1]))
+print("The extension of the file is:", file_extn[-1])
